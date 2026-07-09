189af48225aab2937b6425b901489476b4f3fbf1
tr1zel <cheapwebsites1@gmail.com>
2026-07-09 03:27:38 +0300

    PIXELS-010: добавил форму входа и выхода

--- users/urls.py
+++ users/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
 from django.views.generic import RedirectView, TemplateView
+from django.contrib.auth.views import LoginView, LogoutView
 
 from users import views
 
 app_name = 'users'
 
 urlpatterns = [
-    path('login/', TemplateView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', RedirectView.as_view(pattern_name='bookings:list'), name='logout'),
+    path('login/', LoginView.as_view(template_name='registration/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('register/', views.RegisterView.as_view(), name='register'),
 ]
