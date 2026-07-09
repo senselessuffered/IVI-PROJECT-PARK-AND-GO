2c5dbb8aa3f8ed8cdf64c2a9ee37d832103b3755
tr1zel <cheapwebsites1@gmail.com>
2026-07-09 03:06:46 +0300

    PIXELS-008: добавил форму регистрации пользователя

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,18 @@
 from django.contrib.auth.forms import UserCreationForm
-
-
+from django.contrib.auth import get_user_model
+from django import forms
 class RegisterForm(UserCreationForm):
-    # TODO PIXELS-008
+    email = forms.EmailField(required=True)
+
+    class Meta:   
+        model = get_user_model()
+        fiels = ('username', 'email', 'password1', 'password2')
+    
+    def clean_email(self):
+        email = self.cleaned_data['email']
+
+        if get_user_model().objects.filter(email=email).exists():
+            raise forms.ValidationError('Пользователь с таким именем уже существует.')
+        
+        return email
     pass
