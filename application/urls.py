c5c63ef8f0e2cc9d8e8099335135fec4c60658ee
Ann <chap24u1143@student.bmstu.ru>
2026-07-09 22:21:07 +0300

    change url admin

--- application/urls.py
+++ application/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('secure-adm1n/', admin.site.urls),
     path('accounts/', include('users.urls')),
     path('spots/', include('spots.urls')),
     path('', include('bookings.urls')),
