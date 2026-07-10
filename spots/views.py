6bc144059bd4ae8a4218579fa60ce86ac149e060
vika <ovd24u1023@student.bmstu.ru>
2026-07-10 10:12:44 +0300

    PIXELS-011: added glassmorphism cards, free spots counter with animated status, booking buttons and smooth scroll highlight

--- spots/views.py
+++ spots/views.py
@@ -6,3 +6,10 @@ class ParkingSpotListView(ListView):
     model = ParkingSpot
     template_name = 'spots/parkingspot_list.html'
     context_object_name = 'spots'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        free_spots = ParkingSpot.objects.filter(is_active=True)
+        context['free_spots'] = free_spots
+        context['free_spots_count'] = free_spots.count()
+        return context
