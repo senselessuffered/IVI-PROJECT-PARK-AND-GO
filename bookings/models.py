1bc7e6336e84f5ce50dfd1b292f08367d33394dc
vika <ovd24u1023@student.bmstu.ru>
2026-07-10 06:14:46 +0300

    PIXELS-007: added Reminder model

--- bookings/models.py
+++ bookings/models.py
@@ -7,5 +7,20 @@ class Booking(models.Model):
 
 
 class Reminder(models.Model):
-    # TODO PIXELS-007
-    pass
+    booking = models.OneToOneField(
+        Booking,
+        on_delete=models.CASCADE,
+        related_name='reminder'
+    )
+    scheduled_for = models.DateTimeField()
+    is_sent = models.BooleanField(default=False)
+    sent_at = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Напоминание'
+        verbose_name_plural = 'Напоминания'
+
+    def __str__(self):
+        return f'Напоминание для брони #{self.booking.id}'
+    
