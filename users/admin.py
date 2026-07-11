87cd12d10a75089ac6bd743b6d263045960cfbf5
Ann <chap24u1143@student.bmstu.ru>
2026-07-11 12:54:05 +0300

    custom admin user

--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+
+User = get_user_model()
 
 
-class UserAdmin(admin.ModelAdmin):
-    # TODO PIXELS-018
-    pass
+@admin.register(User)
+class UserAdmin(DjangoUserAdmin):
+    list_display = ("id", "username", "email", "first_name", "last_name", "is_staff", "is_active", "date_joined")
+    list_filter = ("is_staff", "is_active", "date_joined")
+    search_fields = ("username", "email", "first_name", "last_name")
+    ordering = ("-date_joined",)
+    readonly_fields = ("date_joined",)
+
+    fieldsets = (
+        (None, {"fields": ("username", "password")} ),
+        ("Personal info", {"fields": ("first_name", "last_name", "email")} ),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")} ),
+        ("Important dates", {"fields": ("last_login", "date_joined")} ),
+    )
+
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("username", "email", "first_name", "last_name", "password1", "password2", "is_staff", "is_active"),
+        }),
+    )
