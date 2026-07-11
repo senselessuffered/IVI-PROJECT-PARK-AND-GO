f1b5792eee173c71901a4943a5cc5a8dcecbf922
Ann <chap24u1143@student.bmstu.ru>
2026-07-11 13:30:28 +0300

    removig first and last name from custom user admin

--- users/admin.py
+++ users/admin.py
@@ -1,28 +1,23 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
-from django.contrib.auth.admin import UserAdmin as DjangoUserAdmin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-User = get_user_model()
+from users.models import User
 
 
 @admin.register(User)
-class UserAdmin(DjangoUserAdmin):
-    list_display = ("id", "username", "email", "first_name", "last_name", "is_staff", "is_active", "date_joined")
-    list_filter = ("is_staff", "is_active", "date_joined")
-    search_fields = ("username", "email", "first_name", "last_name")
-    ordering = ("-date_joined",)
-    readonly_fields = ("date_joined",)
-
+class UserAdmin(BaseUserAdmin):
+    list_display = ('username', 'email', 'is_staff', 'is_active')
+    list_filter = ('is_staff', 'is_superuser', 'is_active')
+    search_fields = ('username', 'email')
+    ordering = ('username',)
     fieldsets = (
-        (None, {"fields": ("username", "password")} ),
-        ("Personal info", {"fields": ("first_name", "last_name", "email")} ),
-        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")} ),
-        ("Important dates", {"fields": ("last_login", "date_joined")} ),
+        (None, {'fields': ('username', 'password')}),
+        ('Контакты', {'fields': ('email',)}),
+        ('Права', {'fields': ('is_active', 'is_staff', 'is_superuser')}),
     )
-
     add_fieldsets = (
         (None, {
-            "classes": ("wide",),
-            "fields": ("username", "email", "first_name", "last_name", "password1", "password2", "is_staff", "is_active"),
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2'),
         }),
     )
