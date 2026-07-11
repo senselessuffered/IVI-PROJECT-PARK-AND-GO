510cc83cae4de3a5e5de2e447c69aac04c3eecea
Daria <nda24u1022@student.bmstu.ru>
2026-07-11 13:29:23 +0300

    PIXELS-013: add booking detail page logic

--- bookings/views.py
+++ bookings/views.py
@@ -1,32 +1,35 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import RedirectView, TemplateView
+from django.views.generic import DetailView, RedirectView, TemplateView
+
+from bookings.models import Booking
 
 
 class BookingListView(LoginRequiredMixin, TemplateView):
     # TODO PIXELS-012
-    template_name = 'bookings/booking_list.html'
+    template_name = "bookings/booking_list.html"
 
 
-class BookingDetailView(LoginRequiredMixin, RedirectView):
-    # TODO PIXELS-013
-    url = '/orders/'
+class BookingDetailView(LoginRequiredMixin, DetailView):
+    model = Booking
+    template_name = "bookings/booking_detail.html"
+    context_object_name = "booking"
 
 
 class BookingCreateView(LoginRequiredMixin, TemplateView):
     # TODO PIXELS-014
-    template_name = 'bookings/booking_form.html'
+    template_name = "bookings/booking_form.html"
 
 
 class BookingUpdateView(LoginRequiredMixin, TemplateView):
     # TODO PIXELS-015
-    template_name = 'bookings/booking_form.html'
+    template_name = "bookings/booking_form.html"
 
 
 class BookingCancelView(LoginRequiredMixin, RedirectView):
     # TODO PIXELS-016
-    url = '/orders/'
+    url = "/orders/"
 
 
 class BookingDeleteView(LoginRequiredMixin, RedirectView):
     # TODO PIXELS-017
-    url = '/orders/'
+    url = "/orders/"
